6568b32f8385c2d31e87775033186e359426e68c
Maxim Babilo <mbabilo@issart.com>
2019-03-06 11:54:58 +0000

    Update Selenium.java

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -1,6 +1,6 @@
 package com.test;
-        import org.openqa.selenium.WebDriver;
-        import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium {
 
