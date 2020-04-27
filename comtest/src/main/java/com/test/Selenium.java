8f8bea77f1386ced180d9cde272e7c6627638ee0
mayurjs26 <43090732+mayurjs26@users.noreply.github.com>
2020-04-27 12:47:03 +0530

    AB#10

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium {
+    // Azure Devops POC
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\mshinde2\\Downloads\\chromedriver_win32\\chromedriver.exe"); // <-- Change this path
